48cfeae74a5d99f930ee635c6aa33a035d01b46b
Caio Alonso <caio@caioalonso.com>
2016-05-02 23:01:32 -0300

    Exercise 2 from lecture 3

--- homework/03/Golf.hs
+++ homework/03/Golf.hs
@@ -1,8 +1,9 @@
 module Golf where
 
-import Safe
+import Data.List
 import Data.List.Split
 import Data.Maybe
+import Safe
 
 skips :: [a] -> [[a]]
 skips [] = []
@@ -10,3 +11,13 @@ skips x = map (f x) [1..(length x)]
 
 f :: [a] -> Int -> [a]
 f l n = catMaybes (map (headMay . reverse) (takeWhile ((n ==) . length) (chunksOf n l)))
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima = catMaybes . map j . filter g . concat . map inits . tails
+
+g :: [Integer] -> Bool
+g (a:b:c:[])
+  | a < b && b > c = True
+g _ = False
+
+j x = atMay x 1
